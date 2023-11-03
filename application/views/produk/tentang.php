4e0f13ba994c23c6ffe89840321f09bf39b841a0
rantidhanty <rantidhanty@gmail.com>
2023-11-03 15:36:39 +0700

    mengupload project semester 4

---
+++ application/views/produk/tentang.php
@@ -0,0 +1,44 @@
+<div class="container-fluid">
+
+	<!-- Page Heading -->
+	<h1 class="h3 mb-2 text-gray-800">Tentang Kami</h1>
+
+	<!-- DataTales Example -->
+	<div class="card shadow mb-4">
+		<div class="card-header py-3">
+			<h6 class="m-0 font-weight-bold text-primary">Tentang Kami</h6>
+		</div>
+		<div class="card-body">
+			<div class="row">
+				<table class="table table-bordered" width="100%" cellspacing="0">
+					<tr>
+						<td>Nama</td>
+						<td>:</td>
+						<td>Kelompok 4 Wedding Organizer</td>
+					</tr>
+					<tr>
+						<td>Alamat</td>
+						<td>:</td>
+						<td>UBSI KRAMAT 98</td>
+					</tr>
+					<tr>
+						<td>No HP</td>
+						<td>:</td>
+						<td>08123456789</td>
+					</tr>
+					<tr>
+						<td>Email</td>
+						<td>:</td>
+						<td>adminkelompok@gmail.com</td>
+					</tr>
+					<tr>
+						<td>Deskripsi</td>
+						<td>:</td>
+						<td>penyedia jasa wedding serta dekorasi lengkap dengan harga merakyat dan sebagai project SEMESTER 4</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</div>
+
+</div>
